5f766b67301c2773f9414dd0ddf51b84c180b4a9
Derek Ekins <derek@spathi.com>
2011-08-03 20:32:46 +0100

    redirecting to permission denied page when user is logged in but has no permission

--- lib/simple-authorisation/sinatra.rb
+++ lib/simple-authorisation/sinatra.rb
@@ -4,6 +4,7 @@ module Sinatra
   module SinatraAuthorisation
     def self.registered(app)
       app.set :authorisation_login, '/login'
+      app.set :authorisation_permission_denied, '/403'
       app.set :authorisation_current_user, :current_user
       app.set :authorisation_anonymous_user_class, NilClass
 
@@ -16,9 +17,17 @@ module Sinatra
             :anonymous_user_class => options.authorisation_anonymous_user_class,
             :method => request.request_method.downcase.to_sym)
           session[:return_to] = request.fullpath unless request.fullpath.include?('favicon.ico')
-          redirect options.authorisation_login
-          return false
+
+          if user.is_a? options.authorisation_anonymous_user_class
+            redirect options.authorisation_login
+          else
+            redirect options.authorisation_permission_denied
+          end
         end
+      end
+
+      app.get '/403' do
+        "The action you have tried to perform is not available"
       end
     end
 
