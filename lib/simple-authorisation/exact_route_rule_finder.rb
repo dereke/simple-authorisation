dc2adc5c6a1fe9bbaa5af504057d97b671b65111
Derek Ekins <derek@spathi.com>
2011-09-17 20:17:21 +0100

    removed extra logging

--- lib/simple-authorisation/exact_route_rule_finder.rb
+++ lib/simple-authorisation/exact_route_rule_finder.rb
@@ -6,11 +6,9 @@ module Simple
       def initialize(routes)
         super(routes)
         @find_by = [:route_by_wild_card, :route_matches]
-        p @find_by
       end
 
       def route_matches(route_name)
-        p "using route match"
         route_name = URI.parse(route_name).path.gsub(/\/$/, '')
         (@routes.keys.sort.reverse.select { |route| route =~ /#{route_name}\/?/ }).first
       end
