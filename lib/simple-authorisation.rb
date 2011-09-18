1fe46e7af94c4f282c5c358f06404da80853a8ab
Derek Ekins <derek@spathi.com>
2011-09-18 21:19:01 +0100

    added helper module into main require

--- lib/simple-authorisation.rb
+++ lib/simple-authorisation.rb
@@ -1,3 +1,4 @@
 require 'simple-authorisation/route_rule_finder'
 require 'simple-authorisation/authorisation'
+require 'simple-authorisation/authorisation_helper'
 require 'simple-authorisation/sinatra'
